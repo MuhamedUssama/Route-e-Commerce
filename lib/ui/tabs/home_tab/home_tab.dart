8bc6b3a78b36b06ffc37fe54c8fd76a0f19499e1
Mohamed <mohamed.osama200538@gmail.com>
2024-04-19 15:38:26 +0200

    Built  ui to return categories iamges and it's ttles

--- lib/ui/tabs/home_tab/home_tab.dart
+++ lib/ui/tabs/home_tab/home_tab.dart
@@ -1,6 +1,10 @@
+import 'package:e_commerce/di/di.dart';
+import 'package:e_commerce/domain/models/categories/category.dart';
 import 'package:e_commerce/ui/tabs/home_tab/home_tab_view_model.dart';
 import 'package:flutter/material.dart';
-import 'package:get_it/get_it.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'widgets/home_category_widget.dart';
 
 class HomeTab extends StatefulWidget {
   const HomeTab({super.key});
@@ -13,7 +17,63 @@ class _HomeTabState extends State<HomeTab> {
   HomeTabViewModel viewModel = getIt.get<HomeTabViewModel>();
 
   @override
+  void initState() {
+    super.initState();
+    viewModel.initPage();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return BlocConsumer<HomeTabViewModel, HomeTabStates>(
+      bloc: viewModel,
+      buildWhen: (previous, current) {
+        if (current is LoadingState) return false;
+        if (current is ErrorState) return false;
+        return true;
+      },
+      builder: (context, state) {
+        if (state is SuccessState) {
+          return buildSuccessWidget(state.categoryList ?? []);
+        }
+        return Scaffold(
+          appBar: AppBar(title: const Text('Defult Screen')),
+          body: const Center(child: CircularProgressIndicator()),
+        );
+      },
+      listenWhen: (previous, current) {
+        if (current is LoadingState) return true;
+        if (current is ErrorState) return true;
+        return false;
+      },
+      listener: (context, state) {
+        if (state is LoadingState) {
+          // showLoading;
+        }
+        if (state is ErrorState) {
+          // showError;
+        }
+      },
+    );
+  }
+
+  Widget buildSuccessWidget(List<Category>? categoryList) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Route eCommerce")),
+      body: Column(
+        children: [
+          Expanded(
+            child: GridView.builder(
+              scrollDirection: Axis.horizontal,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2),
+              itemBuilder: (context, index) {
+                return HomeCategoryWidget(categoryList: categoryList[index]);
+              },
+              itemCount: categoryList!.length,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
