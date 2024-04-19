f20c586bf68898f18031d829886030c9a37f5feb
Mohamed <mohamed.osama200538@gmail.com>
2024-04-19 12:56:31 +0200

    Created getAllCategories func in ApiManager class to call categories api and get data and created too categoryDataSource contract class

--- lib/data/api/api_manager.dart
+++ lib/data/api/api_manager.dart
@@ -1,7 +1,18 @@
+import 'dart:convert';
+
+import 'package:e_commerce/data/models/categories_response/categories_response.dart';
 import 'package:http/http.dart' as http;
+import 'package:injectable/injectable.dart';
 
+@singleton
 class ApiManager {
-  static const String baseUrl = "https://ecommerce.routemisr.com";
+  static const String baseUrl = "ecommerce.routemisr.com";
 
-  getAllCategories() {}
+  Future<CategoriesResponse> getAllCategories() async {
+    Uri url = Uri.https(baseUrl, "api/v1/categories");
+    http.Response response = await http.get(url);
+    var json = jsonDecode(response.body);
+    CategoriesResponse categoriesResponse = CategoriesResponse.fromJson(json);
+    return categoriesResponse;
+  }
 }
