0d1f7458ef66192befc4f0891cd3debed4aec37a
Mohamed <mohamed.osama200538@gmail.com>
2024-04-21 12:32:56 +0200

    Craeted Sub Categories data model classes in the data layer

---
+++ lib/data/models/sub_categories_response/sub_categories_dto.dart
@@ -0,0 +1,51 @@
+class SubCategoriesDto {
+  SubCategoriesDto({
+    this.id,
+    this.name,
+    this.slug,
+    this.category,
+    this.createdAt,
+    this.updatedAt,
+  });
+
+  SubCategoriesDto.fromJson(dynamic json) {
+    id = json['_id'];
+    name = json['name'];
+    slug = json['slug'];
+    category = json['category'];
+    createdAt = json['createdAt'];
+    updatedAt = json['updatedAt'];
+  }
+  String? id;
+  String? name;
+  String? slug;
+  String? category;
+  String? createdAt;
+  String? updatedAt;
+  SubCategoriesDto copyWith({
+    String? id,
+    String? name,
+    String? slug,
+    String? category,
+    String? createdAt,
+    String? updatedAt,
+  }) =>
+      SubCategoriesDto(
+        id: id ?? this.id,
+        name: name ?? this.name,
+        slug: slug ?? this.slug,
+        category: category ?? this.category,
+        createdAt: createdAt ?? this.createdAt,
+        updatedAt: updatedAt ?? this.updatedAt,
+      );
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['_id'] = id;
+    map['name'] = name;
+    map['slug'] = slug;
+    map['category'] = category;
+    map['createdAt'] = createdAt;
+    map['updatedAt'] = updatedAt;
+    return map;
+  }
+}
