f7fbf6088093fe55201edc811ab0e8b0787286f1
Mohamed Osama <mohamed.osama200538@gmail.com>
2024-04-25 21:55:30 +0200

    Get Products in Products Catalog Screen

--- lib/ui/screens/products/products_by/products_catalog.dart
+++ lib/ui/screens/products/products_by/products_catalog.dart
@@ -1,11 +1,50 @@
+import 'package:e_commerce/di/di.dart';
+import 'package:e_commerce/ui/screens/products/products_by/product_catalog_view_model.dart';
+import 'package:e_commerce/ui/widgets/custom_product_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ProductsCatalog extends StatelessWidget {
   static const String routeName = "productsCatalog";
-  const ProductsCatalog({super.key});
+  ProductsCatalog({super.key});
+
+  final viewModel = getIt.get<ProductsCatalogViewModel>();
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: BlocBuilder<ProductsCatalogViewModel, ProductsCatalogStates>(
+        bloc: viewModel,
+        builder: (context, state) {
+          if (state is LoadingState) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (state is ErrorState) {
+            return Center(
+              child: Text(
+                state.message ?? "Something went worng, please try again",
+              ),
+            );
+          } else if (state is SuccessState) {
+            return GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 2,
+                mainAxisSpacing: 8,
+                childAspectRatio: 8 / 16,
+              ),
+              itemBuilder: (context, index) {
+                return CustomProductWidget(
+                  product: state.products![index],
+                );
+              },
+              itemCount: state.products!.length,
+              scrollDirection: Axis.vertical,
+            );
+          } else {
+            return const SizedBox();
+          }
+        },
+      ),
+    );
   }
 }
