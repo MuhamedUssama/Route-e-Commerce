ebed548cb3ebdf79e84b08f48f5096ca8a77d5fe
Mohamed Osama <mohamed.osama200538@gmail.com>
2024-06-07 13:14:23 +0300

    Finished signup screen

--- lib/ui/utils/theme/app_colors.dart
+++ lib/ui/utils/theme/app_colors.dart
@@ -2,4 +2,6 @@ import 'package:flutter/material.dart';
 
 abstract class AppColors {
   static const Color primaryolor = Color(0XFF06004F);
+  static const Color buttonTextColor = Color(0XFF004182);
+  static const Color white = Colors.white;
 }
