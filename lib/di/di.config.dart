904c4c4dccf0484af8e38cd786697f27afa8a2fe
Mohamed Osama <mohamed.osama200538@gmail.com>
2024-07-22 00:06:09 +0300

    Created login view model class and prepared states to use

--- lib/di/di.config.dart
+++ lib/di/di.config.dart
@@ -15,48 +15,49 @@ import '../data/api/api_manager.dart' as _i5;
 import '../data/data_source/brands_data_source.dart' as _i6;
 import '../data/data_source/category_data_source_contract.dart' as _i10;
 import '../data/data_source/login_data_source.dart' as _i16;
-import '../data/data_source/products_data_source.dart' as _i21;
-import '../data/data_source/sign_up_data_source.dart' as _i23;
+import '../data/data_source/products_data_source.dart' as _i22;
+import '../data/data_source/sign_up_data_source.dart' as _i24;
 import '../data/data_source/sub_categories_on_category_data_source.dart'
-    as _i29;
+    as _i30;
 import '../data/data_source_impl/brands_data_source_impl.dart' as _i7;
 import '../data/data_source_impl/category_data_source_impl.dart' as _i11;
 import '../data/data_source_impl/login_data_source_impl.dart' as _i17;
-import '../data/data_source_impl/products_data_source_impl.dart' as _i22;
-import '../data/data_source_impl/sign_up_data_source_impl.dart' as _i24;
+import '../data/data_source_impl/products_data_source_impl.dart' as _i23;
+import '../data/data_source_impl/sign_up_data_source_impl.dart' as _i25;
 import '../data/data_source_impl/sub_categories_on_category_data_source_impl.dart'
-    as _i30;
+    as _i31;
 import '../data/repository_impl/brands_repository_impl.dart' as _i9;
 import '../data/repository_impl/category_repository_impl.dart' as _i13;
 import '../data/repository_impl/login_repository_impl.dart' as _i19;
-import '../data/repository_impl/product_repository_impl.dart' as _i36;
-import '../data/repository_impl/sign_up_repository_impl.dart' as _i26;
+import '../data/repository_impl/product_repository_impl.dart' as _i37;
+import '../data/repository_impl/sign_up_repository_impl.dart' as _i27;
 import '../data/repository_impl/sub_categories_on_category_repository_impl.dart'
-    as _i32;
+    as _i33;
 import '../domain/repository/brand_repository_contract.dart' as _i8;
 import '../domain/repository/category_repository_contract.dart' as _i12;
 import '../domain/repository/login_repository.dart' as _i18;
-import '../domain/repository/product_repository_contract.dart' as _i35;
-import '../domain/repository/sign_up_repository.dart' as _i25;
+import '../domain/repository/product_repository_contract.dart' as _i36;
+import '../domain/repository/sign_up_repository.dart' as _i26;
 import '../domain/repository/sub_categories_on_category_repository_contract.dart'
-    as _i31;
+    as _i32;
 import '../domain/use_cases/get_brands_usecase.dart' as _i14;
 import '../domain/use_cases/get_categories_usecase.dart' as _i15;
-import '../domain/use_cases/get_most_selling_products_usecase.dart' as _i38;
-import '../domain/use_cases/get_products_catalog.dart' as _i39;
+import '../domain/use_cases/get_most_selling_products_usecase.dart' as _i39;
+import '../domain/use_cases/get_products_catalog.dart' as _i40;
 import '../domain/use_cases/get_sub_categories_on_category_usecase.dart'
-    as _i34;
+    as _i35;
 import '../domain/use_cases/login_usecase.dart' as _i20;
-import '../domain/use_cases/sign_up_usecase.dart' as _i27;
+import '../domain/use_cases/sign_up_usecase.dart' as _i28;
 import '../helpers/network_connection.dart' as _i4;
-import '../ui/screens/auth/sign_up/sign_up_view_model.dart' as _i28;
+import '../ui/screens/auth/login/login_view_model.dart' as _i21;
+import '../ui/screens/auth/sign_up/sign_up_view_model.dart' as _i29;
 import '../ui/screens/products/products_by/product_catalog_view_model.dart'
-    as _i41;
+    as _i42;
 import '../ui/screens/tabs/categories_tab/cubits/categories/categories_tab_view_model.dart'
-    as _i33;
+    as _i34;
 import '../ui/screens/tabs/categories_tab/cubits/sub_categories/sub_categories_on_category_view_model.dart'
-    as _i37;
-import '../ui/screens/tabs/home_tab/home_tab_view_model.dart' as _i40;
+    as _i38;
+import '../ui/screens/tabs/home_tab/home_tab_view_model.dart' as _i41;
 import '../ui/screens/tabs/main_screen_view_model.dart' as _i3;
 
 extension GetItInjectableX on _i1.GetIt {
@@ -92,43 +93,45 @@ extension GetItInjectableX on _i1.GetIt {
         () => _i19.LoginRepositoryImpl(gh<_i16.LoginDataSource>()));
     gh.factory<_i20.LoginUsecase>(
         () => _i20.LoginUsecase(gh<_i18.LoginRepository>()));
-    gh.factory<_i21.ProductsDataSource>(
-        () => _i22.ProductDataSourceImpl(gh<_i5.ApiManager>()));
-    gh.factory<_i23.SignUpDataSource>(
-        () => _i24.SignUpDataSourceImpl(gh<_i5.ApiManager>()));
-    gh.factory<_i25.SignUpRepository>(
-        () => _i26.SignUpRepositoryImpl(gh<_i23.SignUpDataSource>()));
-    gh.factory<_i27.SignUpUseCase>(
-        () => _i27.SignUpUseCase(gh<_i25.SignUpRepository>()));
-    gh.factory<_i28.SignUpViewModel>(
-        () => _i28.SignUpViewModel(gh<_i27.SignUpUseCase>()));
-    gh.factory<_i29.SubCategoriesOnCategoryDataSource>(
-        () => _i30.SubCategoriesOnCategoryDataSourceImpl(gh<_i5.ApiManager>()));
-    gh.factory<_i31.SubCategoriesOnCategoryRepositoryContract>(() =>
-        _i32.SubCategoriesOnCategoryRepoImpl(
-            gh<_i29.SubCategoriesOnCategoryDataSource>()));
-    gh.factory<_i33.CategoriesTabViewModel>(
-        () => _i33.CategoriesTabViewModel(gh<_i15.GetCategoriesUseCase>()));
-    gh.factory<_i34.GetSubCategoriesOnCategoryUsecase>(() =>
-        _i34.GetSubCategoriesOnCategoryUsecase(
-            gh<_i31.SubCategoriesOnCategoryRepositoryContract>()));
-    gh.factory<_i35.ProductRepositoryContract>(
-        () => _i36.ProductRepositoryImpl(gh<_i21.ProductsDataSource>()));
-    gh.factory<_i37.SubCategoriesOnCategoryViewModel>(() =>
-        _i37.SubCategoriesOnCategoryViewModel(
-            gh<_i34.GetSubCategoriesOnCategoryUsecase>()));
-    gh.factory<_i38.GetMostSellingProductsUseCase>(() =>
-        _i38.GetMostSellingProductsUseCase(
-            gh<_i35.ProductRepositoryContract>()));
-    gh.factory<_i39.GetProductsCatalogUseCase>(() =>
-        _i39.GetProductsCatalogUseCase(gh<_i35.ProductRepositoryContract>()));
-    gh.factory<_i40.HomeTabViewModel>(() => _i40.HomeTabViewModel(
+    gh.factory<_i21.LoginViewModel>(
+        () => _i21.LoginViewModel(gh<_i20.LoginUsecase>()));
+    gh.factory<_i22.ProductsDataSource>(
+        () => _i23.ProductDataSourceImpl(gh<_i5.ApiManager>()));
+    gh.factory<_i24.SignUpDataSource>(
+        () => _i25.SignUpDataSourceImpl(gh<_i5.ApiManager>()));
+    gh.factory<_i26.SignUpRepository>(
+        () => _i27.SignUpRepositoryImpl(gh<_i24.SignUpDataSource>()));
+    gh.factory<_i28.SignUpUseCase>(
+        () => _i28.SignUpUseCase(gh<_i26.SignUpRepository>()));
+    gh.factory<_i29.SignUpViewModel>(
+        () => _i29.SignUpViewModel(gh<_i28.SignUpUseCase>()));
+    gh.factory<_i30.SubCategoriesOnCategoryDataSource>(
+        () => _i31.SubCategoriesOnCategoryDataSourceImpl(gh<_i5.ApiManager>()));
+    gh.factory<_i32.SubCategoriesOnCategoryRepositoryContract>(() =>
+        _i33.SubCategoriesOnCategoryRepoImpl(
+            gh<_i30.SubCategoriesOnCategoryDataSource>()));
+    gh.factory<_i34.CategoriesTabViewModel>(
+        () => _i34.CategoriesTabViewModel(gh<_i15.GetCategoriesUseCase>()));
+    gh.factory<_i35.GetSubCategoriesOnCategoryUsecase>(() =>
+        _i35.GetSubCategoriesOnCategoryUsecase(
+            gh<_i32.SubCategoriesOnCategoryRepositoryContract>()));
+    gh.factory<_i36.ProductRepositoryContract>(
+        () => _i37.ProductRepositoryImpl(gh<_i22.ProductsDataSource>()));
+    gh.factory<_i38.SubCategoriesOnCategoryViewModel>(() =>
+        _i38.SubCategoriesOnCategoryViewModel(
+            gh<_i35.GetSubCategoriesOnCategoryUsecase>()));
+    gh.factory<_i39.GetMostSellingProductsUseCase>(() =>
+        _i39.GetMostSellingProductsUseCase(
+            gh<_i36.ProductRepositoryContract>()));
+    gh.factory<_i40.GetProductsCatalogUseCase>(() =>
+        _i40.GetProductsCatalogUseCase(gh<_i36.ProductRepositoryContract>()));
+    gh.factory<_i41.HomeTabViewModel>(() => _i41.HomeTabViewModel(
           gh<_i15.GetCategoriesUseCase>(),
           gh<_i14.GetBrandsUseCase>(),
-          gh<_i38.GetMostSellingProductsUseCase>(),
+          gh<_i39.GetMostSellingProductsUseCase>(),
         ));
-    gh.factory<_i41.ProductsCatalogViewModel>(() =>
-        _i41.ProductsCatalogViewModel(gh<_i39.GetProductsCatalogUseCase>()));
+    gh.factory<_i42.ProductsCatalogViewModel>(() =>
+        _i42.ProductsCatalogViewModel(gh<_i40.GetProductsCatalogUseCase>()));
     return this;
   }
 }
--- lib/ui/screens/auth/login/login_view_model.dart
+++ lib/ui/screens/auth/login/login_view_model.dart
@@ -1,6 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:injectable/injectable.dart';
 import 'package:e_commerce/domain/models/login/login_dto.dart';
+import 'package:e_commerce/domain/use_cases/login_usecase.dart';
+
+@injectable
+class LoginViewModel extends Cubit<LoginStates> {
+  LoginUsecase usecase;
+
+  @factoryMethod
+  LoginViewModel(this.usecase) : super(LoginInitialState());
+
+  var formKey = GlobalKey<FormState>();
+  bool isObscurePassword = true;
+
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  Future<void> login() async {
+    emit(LoginInitialState());
+
+    if (formKey.currentState?.validate() == true) {
+      emit(LoginLoadingState());
+
+      final either = await usecase.invoke(
+        emailController.text,
+        passwordController.text,
+      );
+
+      either!.fold(
+        (failure) => emit(LoginErrorState(failure.errorMessage)),
+        (userResponse) => emit(LoginSuccessState(userResponse)),
+      );
+    }
+  }
+}
 
 abstract class LoginStates {}
+
+class LoginInitialState extends LoginStates {}
 
 class LoginLoadingState extends LoginStates {}
 
