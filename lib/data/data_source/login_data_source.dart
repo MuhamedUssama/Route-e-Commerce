f3eecceb5401a8952d9146b3fa3ba56a6ddfb848
Mohamed Osama <mohamed.osama200538@gmail.com>
2024-07-21 23:01:26 +0300

    Created login data source contract class

--- lib/data/data_source/login_data_source.dart
+++ lib/data/data_source/login_data_source.dart
@@ -2,6 +2,6 @@ import 'package:dartz/dartz.dart';
 import 'package:e_commerce/domain/failures.dart';
 import 'package:e_commerce/domain/models/login/login_dto.dart';
 
-abstract class LoginRepository {
+abstract class LoginDataSource {
   Future<Either<Failures, LoginDto>>? login(String email, String password);
 }
